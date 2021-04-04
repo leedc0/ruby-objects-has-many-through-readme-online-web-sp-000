c4cea5a6905af72c7b94f2cc8b8c982277220e05
leedc0 <leedc@me.com>
2021-04-03 21:43:34 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,55 @@
+require 'pry'
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  # initializes with a name and age
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  # is class method that returns the contents of @@all
+  def self.all
+    @@all
+  end
+
+  # initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  # returns an Array of Meal instances associated with this customer
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  # returns an Array of Waiter instances associated with this customer's meals
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  # initializes with a name and years of experience
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  # is class method that returns the contents of @@all
+  def self.all
+    @@all
+  end
+
+  # initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  # returns an Array of Meal instances associated with this waiter
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
+  end
+
+  # returns the Customer instance associated with the meal that received the largest tip
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  
+    best_tipped_meal.customer
+  end
 
 end
